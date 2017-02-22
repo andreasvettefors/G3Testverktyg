b4d34521b38572b575ce720bbbae049f7d171c93
Andreas Vettefors <contact@vettefors.se>
2017-02-22 10:21:42 +0100

    Tog bort klasserna från load-list

--- load-list.js
+++ load-list.js
@@ -24,27 +24,11 @@ window.load = `
   js/classes/
     bootstrap-size.js
     modal.class.js
-    organism.class.js  
-    pet.class.js
-    pet-owner.class.js
-    pet-list.class.js
-    pet-owner-list.class.js
-    pet-owner-view.class.js
-    test-data-generator.class.js
     app.class.js
-    navbar.class.js
-    startpage.class.js
-    about-us.class.js
 
   templates/
     bootstrap-size.html
     modal.html
-    pet.html
-    pet-owner.html
-    pet-owner-view.html
-    navbar.html
-    startpage.html
-    about-us.html
 
   js/main.js
 
