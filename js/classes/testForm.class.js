0074f208269b8cf36dd2eb4ae4943651129c8bbb
Ramona Chantaf <ramonach.91@gmail.com>
2017-02-22 10:41:51 +0100

    lagt till ny class i js med namnet testForm.class.js

---
+++ js/classes/testForm.class.js
@@ -0,0 +1,24 @@
+class TestForm extends Base{
+
+	static defaultPropertyValues(){
+		return{
+			idTest: 0,
+			title:'Programmering A',
+			//description:''
+			question: ['question1','question2']
+
+		}
+		
+	}
+
+	constructor(propertyValues = {}){
+		super(propertyValues);
+	} 
+
+ message(){
+
+ 	console.log('hej');
+ }
+	
+  
+} 
