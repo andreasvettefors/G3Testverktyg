f416c7773888420293cfca42c4bca4519984ef51
Andreas Vettefors <contact@vettefors.se>
2017-02-24 13:46:00 +0100

    En funktionell app

--- js/classes/app.class.js
+++ js/classes/app.class.js
@@ -3,17 +3,13 @@ class App {
 	constructor() {
 		new BootstrapSize().display('body');
 
-		var test = new TestForm();
-    
-	   
-    //hämtar question och answerAlternative från databasen 
-		test.questions.readAllFromDb(1,() => {
+
+		//test.questions[0].answerOptions.readFromDb(1, ()=>{
       
-    });
+      //});
+		
+	
 
-
-    window.test = test;
-    
 		new Login().display('body');
 	}
 }
