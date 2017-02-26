90e5a249659e2f0ed97c7eb89473899939680e93
Andreas Vettefors <contact@vettefors.se>
2017-02-26 21:01:06 +0100

    Kommenterade ut bootstrapsize

--- js/classes/app.class.js
+++ js/classes/app.class.js
@@ -2,17 +2,7 @@ class App {
 
 	constructor() {
 
-        /*
-           HUR MAN KALLAR CALLBACKS METODER
-           var a = new studentAnswer();
-      a.studentGradePercentage(1,1, (element) => {
-            //VÄRDET FINNS I "ELEMENT"
-				console.log(element);
-          }); 
-          */
-    	
-
-		new BootstrapSize().display('body');
+		//new BootstrapSize().display('body');
 		
 		new Login().display('body');
 	}
