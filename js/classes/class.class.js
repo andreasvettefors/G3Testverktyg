1c07c1a47d6e5ceb8415de7d80643e0e92933726
ConradLetelier <Conrad@Letelier.email>
2017-03-02 09:46:05 +0100

    Adderat en function som räknar ut medelvärde på klassens testresultat. Loggar just nu resultatet bara

--- js/classes/class.class.js
+++ js/classes/class.class.js
@@ -18,9 +18,32 @@ class Class extends Base {
 		sl.readStudentData(this.idClasses,()=>{
 			$(e.target).after(sl.display());
 			//sl.display('.students');
+                 this.avarageGradeCalculator(1,(element) => {
+				console.log(element);
+          }); 
 			window.sl = sl;
 		});
 	}
-
-
+    avarageGradeCalculator(testID,callback){
+        this.getStudentGrades(this.idClasses,testID,(element) => {
+                var avgGrade=0;
+                  for(var i = 0; i < element.length;i++){
+                   var grade=parseInt(element[i].text);
+                      avgGrade+=grade;
+                }
+                avgGrade/=element.length;
+                return callback(avgGrade);
+          }); 
+    }
+	getStudentGrades(classID,testID,callback) {
+        this.db.getGrades([classID,testID],(data) => {
+				return callback(data);
+			});
+		};
+    static get sqlQueries(){
+			return {
+				getGrades: `
+    select text from grade,users where user_idUser = idUser && classes_idClasses = ? && test_idTest =?`
+        }
+    }
 }
